c98cfae618c3ff10af7d11b1ca069084a2466f8a
benjadahl <benjadahl@gmail.com>
2016-07-15 00:21:14 +0200

    Added gitignore and Hasher.py

--- CLI.py
+++ CLI.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import Hasher
 
 if __name__ == '__main__':
     if sys.argv[1] == "string":
@@ -8,4 +9,5 @@ if __name__ == '__main__':
 
     if sys.argv[1] == "int":
         source = "{0:b}".format(int(sys.argv[2]))
-        print(source)
+        source = [source[i:i+8] for i in range(0, len(source), 8)]
+        Hasher.hash(source)
