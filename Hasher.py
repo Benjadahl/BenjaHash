ac2ef38b250bd83d47a5464bc6fde20227e034a4
benjadahl <benjadahl@gmail.com>
2016-07-15 00:36:18 +0200

    Started making hashing loop

--- Hasher.py
+++ Hasher.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
 def hash(source):
-    for row in source:
-        print(row)
+    for i in range(0, len(source), 2):
+        print(i)
+        print(str(int(source[i], 2) % int(source[i + 1], 2)))
