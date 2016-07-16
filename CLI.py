289ec64cfadb8c14db767c9988cd4d169e91982f
benjadahl <benjadahl@gmail.com>
2016-07-16 19:13:21 +0200

    Organized project further by seperating into files and functions, the hasher is in a usable state now

--- CLI.py
+++ CLI.py
@@ -2,12 +2,12 @@
 
 import sys
 import Hasher
+import intToBinary
 
 if __name__ == '__main__':
     if sys.argv[1] == "string":
         print("String does not work yet")
 
     if sys.argv[1] == "int":
-        source = "{0:b}".format(int(sys.argv[2]))
-        source = [source[i:i+8] for i in range(0, len(source), 8)]
+        source = intToBinary.convert(sys.argv[2])
         Hasher.hash(source)
--- Hasher.py
+++ Hasher.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
+import intToBinary
+
+#Modulus_Always_PoSitive
+def MAPS(value1, value2):
+    if value1 > value2:
+        return value1 % value2
+    else:
+        return value2 % value1
+
+def HashLoop(binary):
+    result = ""
+    for i in range(0, len(binary), 2):
+        try:
+            #result = result + (int(source[i], 2) % int(source[i + 1], 2))
+            result = result + str(MAPS(int(binary[i], 2),int(binary[i + 1], 2)))
+        except ZeroDivisionError:
+            print("zero error")
+        except IndexError:
+            print("index error")
+    return(result)
+
 def hash(source):
-    for i in range(0, len(source), 2):
-        print(i)
-        print(str(int(source[i], 2) % int(source[i + 1], 2)))
+    result = HashLoop(source)
+    print (result)
+
+    while len(result) > 1:
+        #print (len(result))
+        result = HashLoop(intToBinary.convert(result))
+        print (result)
