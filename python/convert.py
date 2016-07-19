d5924fba2f471a3ba2bc25a0149e01b8ae8bd568
benjadahl <benjadahl@gmail.com>
2016-07-20 00:45:08 +0200

    Seperated functions of the converter file, bin input is still a WIP

--- python/convert.py
+++ python/convert.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
-def intToBinary(arg):
-        C = "{0:b}".format(int(arg))
-        C = [C[i:i+8] for i in range(0, len(C), 8)]
+def intToBinary(x):
+        C = "{0:b}".format(int(x))
+        C = binToArray(C)
         return C
+
+#This feature is a Work In Progress
+def binToArray(x):
+    C = [x[i:i+8] for i in range(0, len(x), 8)]
+    return C
--- python/terminal.py
+++ python/terminal.py
@@ -11,3 +11,6 @@ if __name__ == '__main__':
     if sys.argv[1] == "int":
         source = convert.intToBinary(sys.argv[2])
         BenjaHash.hash(source)
+
+    if sys.argv[1] == "bin":
+        BenjaHash.hash(str(convert.binToArray(sys.argv[2])))
