ca1de5215a0c936ddf9c7ecfe8c1fbadbab96382
benjadahl <benjadahl@gmail.com>
2016-07-22 23:25:59 +0200

    Fixed ident issue in convert.py

--- demo/convert.py
+++ demo/convert.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 def intToBinary(x):
-        C = "{0:b}".format(int(x))
-        C = binToArray(C)
-        return C
+    C = "{0:b}".format(int(x))
+    C = binToArray(C)
+    return C
 
 def strToBinary(x):
     C = ''.join(format(ord(i), 'b') for i in x)
